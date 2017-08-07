f3698b8d04f50d740f690b5ee01a9d9a53f8fcb9
JUXINbit <632464297@qq.com>
2017-08-08 02:11:31 +0800

    模拟实现STLstring容器

--- MyStringwithCOW.cpp
+++ MyStringwithCOW.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 #include<string>
+#include<assert.h>
 using namespace std;
 
 // MyString  
@@ -49,7 +50,7 @@ void PrintMyString(const MyString& s)
 	cout << s._str << endl;
 }
 
-// MyString+Copy-On-Write 
+// MyString+Copy-On-Write ,  当发生拷贝或者赋值的时候将引用计数++！ 
 class MyCOWString
 {
 private:
@@ -70,15 +71,50 @@ public:
 		cout << "拷贝构造" << endl;
 		(*_refCount)++;
 	}
+	//MyCOWString& operator=(const MyCOWString& s)      //這样的方案是错误的，需要考虑到是否需要释放之前的空间
+	//{
+	//	if (this != &s)
+	//	{
+	//		_str = s._str;
+	//		_refCount = s._refCount;
+	//		(*_refCount)++;
+	//	}
+	//	return *this;
+	//}
+
 	MyCOWString& operator=(const MyCOWString& s)
 	{
 		if (this != &s)
 		{
+			if (--(*_refCount) == 0)           //考虑被赋值的对象的资源有可能还被其他对象占有；当--计数器为0的时候，释放空间
+			{
+				delete[] _str;
+				_str = NULL;
+			}
 			_str = s._str;
 			_refCount = s._refCount;
 			(*_refCount)++;
 		}
 		return *this;
+	}
+	const char& operator[](size_t index) const        //下标引用的重载  只读
+	{
+		assert(index < strlen(_str));
+		return _str[index];
+	}
+	char& operator[](size_t index)                    //切记返回引用
+	{
+		assert(index < strlen(_str));
+		if (--(*_refCount) != 0)                      //注意這里也需要注意对象的引用计数个数
+		{
+			if (_str)
+			{
+				char* tmp = new char[strlen(_str) + 1];     //這里就是写时拷贝的体现啦
+				strcpy(tmp, _str);
+				_str = tmp;
+			}
+		}
+		return _str[index];
 	}
 	~MyCOWString()
 	{
@@ -103,11 +139,20 @@ public:
 void MyCOWStringTest(void)
 {
 	MyCOWString s1("Hello COW");
-	MyCOWString s2(s1);
-	s2.PrintString();
 	MyCOWString s3("Hello String");
-	s1 = s3;
-	s2.PrintString();
+	s3 = s1;
+	s1[1] = 'o';
+	//MyCOWString s2(s1);
+	//s2.PrintString();
+	//MyCOWString s3("Hello String");
+	//s2.PrintString();
+	//MyCOWString s4(s1);
+	//MyCOWString s5(s1);
+	//s4 = s1;
+	//s4.PrintString();
+	//cout << s1[1] << endl;
+	s1.PrintString();
+	s3.PrintString();
 }
 
 void MyStringTest(void)
